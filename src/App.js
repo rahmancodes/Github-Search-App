31924f825c9ed6f6870afe6f8dbc9eb046d4c12f
Rahman Oke <abdulrahmanoke170@gmail.com>
2022-11-05 20:16:33 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css'; 
 import Home from './components/pages/Home/Home';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Repos from "./components/pages/Repos/Repos";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/repos" element={<Repos />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/pages/Repos/Repos.js
@@ -0,0 +1,102 @@
+import React from "react"; 
+import { Link } from "react-router-dom";
+import "./Repos.css";
+const Repos = () => {
+    return ( 
+        <div className="container">
+            <Link to="/" className="back">
+                Back
+            </Link> 
+            <div className="user-repos">
+                <div className="repo">
+                    <h3>
+                        <a href="#">Name of the Repo</a>
+                    </h3> 
+                    <p>
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                    </p> 
+                    <p> 
+                        <small>Written in JavaScript</small>
+                    </p>
+                </div>
+                <div className="repo">
+                    <h3>
+                        <a href="#">Name of the Repo</a>
+                    </h3> 
+                    <p>
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                    </p> 
+                    <p> 
+                        <small>Written in JavaScript</small>
+                    </p>
+                </div>
+                <div className="repo">
+                    <h3>
+                        <a href="#">Name of the Repo</a>
+                    </h3> 
+                    <p>
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                    </p> 
+                    <p> 
+                        <small>Written in JavaScript</small>
+                    </p>
+                </div>
+                <div className="repo">
+                    <h3>
+                        <a href="#">Name of the Repo</a>
+                    </h3> 
+                    <p>
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                    </p> 
+                    <p> 
+                        <small>Written in JavaScript</small>
+                    </p>
+                </div>
+                <div className="repo">
+                    <h3>
+                        <a href="#">Name of the Repo</a>
+                    </h3> 
+                    <p>
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                    </p> 
+                    <p> 
+                        <small>Written in JavaScript</small>
+                    </p>
+                </div>
+                <div className="repo">
+                    <h3>
+                        <a href="#">Name of the Repo</a>
+                    </h3> 
+                    <p>
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                        QEQWFxpGR0kACRRVWVwLDBCSkpQAABE0goBbeasjdfkfjjjff
+                    </p> 
+                    <p> 
+                        <small>Written in JavaScript</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+    ) 
+    
+} 
+export default Repos
